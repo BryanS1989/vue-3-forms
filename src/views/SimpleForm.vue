2733169589a652c1d86814c60290ed1a93ecdacf
BryanS1989 <bryanstevenal@gmail.om>
2024-05-12 22:07:34 +0200

    BaseSelect

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -2,17 +2,7 @@
     <div>
         <h1>Create an event</h1>
         <form>
-            <label>Select a category</label>
-            <select v-model="event.category">
-                <option
-                    v-for="option in categories"
-                    :value="option"
-                    :key="option"
-                    :selected="option === event.category"
-                >
-                    {{ option }}
-                </option>
-            </select>
+            <BaseSelect :options="categories" v-mode="event.category" label="Select a category" />
 
             <h3>Name & describe your event</h3>
 
@@ -53,6 +43,7 @@
 
 <script>
 import BaseInput from '@/components/BaseInput.vue';
+import BaseSelect from '@/components/BaseSelect.vue';
 
 export default {
     data() {
@@ -80,7 +71,8 @@ export default {
         };
     },
     components: {
-        BaseInput
+        BaseInput,
+        BaseSelect
     }
 };
 </script>
